679ddc09abbee458623efc138bb809432531bac0
AnilP. <anl.pirincoglu@gmail.com>
2024-03-05 05:32:47 +0300

    add list of category converter method

--- src/main/java/com/challenge/backend/ecommerce/util/ClassConverter.java
+++ src/main/java/com/challenge/backend/ecommerce/util/ClassConverter.java
@@ -3,8 +3,17 @@ package com.challenge.backend.ecommerce.util;
 import com.challenge.backend.ecommerce.dto.CategoryResponseDto;
 import com.challenge.backend.ecommerce.entity.Category;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClassConverter {
     public static CategoryResponseDto categoryToDto(Category category) {
         return new CategoryResponseDto(category.getName(), category.getDescription());
+    }
+
+    public static List<CategoryResponseDto> listOfcategoryToDto(List<Category> categories) {
+        return new ArrayList<>(categories.stream()
+                .map(ClassConverter::categoryToDto)
+                .toList());
     }
 }
