d9047872b0361630749d71bf44d50818e5c76c53
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-10-13 21:43:04 +0300

    Add UserService for user retrieval
    Created UserService class to handle user-related operations. This includes methods for fetching user details by ID, leveraging UserRepository and UserMapper. Added exception handling for missing users using ResourceNotFoundException.

---
+++ src/main/java/dev/anilp/ecommerce_backend/servise/UserService.java
@@ -0,0 +1,33 @@
+package dev.anilp.ecommerce_backend.servise;
+
+import dev.anilp.ecommerce_backend.dto.user.UserResponseDTO;
+import dev.anilp.ecommerce_backend.entity.user.User;
+import dev.anilp.ecommerce_backend.exception.exception_class.ResourceNotFoundException;
+import dev.anilp.ecommerce_backend.mapper.UserMapper;
+import dev.anilp.ecommerce_backend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import static dev.anilp.ecommerce_backend.exception.ExceptionConstant.ID;
+import static dev.anilp.ecommerce_backend.exception.ExceptionConstant.USER;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final UserMapper mapper;
+
+    public UserService(UserRepository userRepository, UserMapper mapper) {
+        this.userRepository = userRepository;
+        this.mapper = mapper;
+    }
+
+    public UserResponseDTO getUserById(Long userId) {
+        return mapper.userToResponse(
+                findUserById(userId)
+        );
+    }
+
+    public User findUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(USER, ID, id.toString()));
+    }
+}
