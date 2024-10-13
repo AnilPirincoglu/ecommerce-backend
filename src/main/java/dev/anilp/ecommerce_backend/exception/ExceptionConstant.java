5d41d72fd8bb33fdcfe844e3686f55eca7381bc4
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-10-13 21:41:22 +0300

    Add ExceptionConstant class with constant values
    This commit introduces a new ExceptionConstant class in the exception package. It defines constants for user-related identifiers such as USER, ADDRESS, EMAIL, ID, and PHONE_NUMBER. These constants will be used across the application for consistency in exception handling.

---
+++ src/main/java/dev/anilp/ecommerce_backend/exception/ExceptionConstant.java
@@ -0,0 +1,10 @@
+package dev.anilp.ecommerce_backend.exception;
+
+public class ExceptionConstant {
+    public static final String USER = "USER";
+    public static final String ADDRESS = "ADDRESS";
+    public static final String EMAIL = "EMAIL";
+    public static final String ID = "ID";
+    public static final String PHONE_NUMBER = "PHONE NUMBER";
+
+}
