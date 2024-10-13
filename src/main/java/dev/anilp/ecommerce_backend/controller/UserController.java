1186b5b0fa829eaf0a6351d5a87fe1fcdcc44fa8
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-10-13 22:02:19 +0300

    Add UserController with CRUD endpoints for users
    This commit introduces the UserController class to handle user operations. It includes endpoints for creating, reading, updating, and deleting user records. This addition will help manage user data through RESTful API requests.

---
+++ src/main/java/dev/anilp/ecommerce_backend/controller/UserController.java
@@ -0,0 +1,59 @@
+package dev.anilp.ecommerce_backend.controller;
+
+import dev.anilp.ecommerce_backend.dto.user.CreateUserRequestDTO;
+import dev.anilp.ecommerce_backend.dto.user.UpdateUserRequestDTO;
+import dev.anilp.ecommerce_backend.dto.user.UserResponseDTO;
+import dev.anilp.ecommerce_backend.servise.UserService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("{userId}")
+    public UserResponseDTO getUserById(@PathVariable Long userId) {
+        return userService.getUserById(userId);
+    }
+
+    @GetMapping("emails/{email}")
+    public UserResponseDTO getUserByEmail(@PathVariable String email) {
+        return userService.getUserByEmail(email);
+    }
+
+    @GetMapping
+    public List<UserResponseDTO> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public void createUser(@Valid @RequestBody CreateUserRequestDTO createUser) {
+        userService.createUser(createUser);
+    }
+
+    @PutMapping("{userId}")
+    public void updateUser(@PathVariable Long userId,
+                           @Valid @RequestBody UpdateUserRequestDTO updateUser) {
+        userService.updateUser(userId, updateUser);
+    }
+
+    @DeleteMapping("{userId}")
+    public void deleteUser(@PathVariable Long userId) {
+        userService.deleteUser(userId);
+    }
+
+}
