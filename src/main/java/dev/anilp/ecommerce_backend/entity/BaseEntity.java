a937ae4349481f3a61e3875747c31fe6f965b403
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-10-13 21:22:26 +0300

    Add BaseEntity with auditing fields

---
+++ src/main/java/dev/anilp/ecommerce_backend/entity/BaseEntity.java
@@ -0,0 +1,25 @@
+package dev.anilp.ecommerce_backend.entity;
+
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public class BaseEntity {
+
+    protected LocalDateTime createdAt;
+    protected LocalDateTime updatedAt;
+
+    @PrePersist
+    public void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
