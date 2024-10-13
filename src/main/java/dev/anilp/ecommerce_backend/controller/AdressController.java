0f3c462e2cb8c21643151e2baf9d576a59092af5
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-10-13 22:02:46 +0300

    Add AddressController class
    Implemented AddressController to handle CRUD operations for user addresses. This includes methods to create, read, update, and delete addresses associated with users through the AddressService.

---
+++ src/main/java/dev/anilp/ecommerce_backend/controller/AdressController.java
@@ -0,0 +1,58 @@
+package dev.anilp.ecommerce_backend.controller;
+
+import dev.anilp.ecommerce_backend.dto.address.AddressResponseDTO;
+import dev.anilp.ecommerce_backend.dto.address.CreateAddressRequestDTO;
+import dev.anilp.ecommerce_backend.dto.address.UpdateAddressRequestDTO;
+import dev.anilp.ecommerce_backend.servise.AddressService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/addresses")
+public class AdressController {
+
+    private final AddressService addressService;
+
+    public AdressController(AddressService addressService) {
+        this.addressService = addressService;
+    }
+
+    @GetMapping("{addressId}/users/{userId}")
+    public AddressResponseDTO getAddressOfUserById(@PathVariable Long userId,
+                                                   @PathVariable Long addressId) {
+        return addressService.getAddressOfUserById(userId, addressId);
+    }
+
+    @GetMapping("users/{userId}")
+    public List<AddressResponseDTO> getAddressesOfUser(@PathVariable Long userId) {
+        return addressService.getAddressesOfUser(userId);
+    }
+
+    @PostMapping("users/{userId}")
+    public void addAddressToUser(@PathVariable Long userId,
+                                 @Valid @RequestBody CreateAddressRequestDTO createAddress) {
+        addressService.addAddressToUser(userId, createAddress);
+    }
+
+    @PutMapping("{addressId}/users/{userId}")
+    public void updateAddressOfUser(@PathVariable Long userId,
+                                    @PathVariable Long addressId,
+                                    @Valid @RequestBody UpdateAddressRequestDTO updateAddress) {
+        addressService.updateAddressOfUser(userId, addressId, updateAddress);
+    }
+
+    @DeleteMapping("{addressId}/users/{userId}")
+    public void deleteAddressFromUser(@PathVariable Long userId,
+                                      @PathVariable Long addressId) {
+        addressService.deleteAddressFromUser(userId, addressId);
+    }
+}
