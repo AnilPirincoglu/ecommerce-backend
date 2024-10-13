b66ee02200a2052fd197b32d716fb3378d928411
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-10-13 21:25:59 +0300

    Add PhoneNumber entity with associated PhoneType enum
    Introduced the PhoneNumber entity to represent user phone numbers with validation constraints. Also, added a PhoneType enum to categorize phone numbers as HOME, WORK, or MOBILE. Both additions include necessary annotations for persistence and validation.

---
+++ src/main/java/dev/anilp/ecommerce_backend/entity/phone_number/PhoneNumber.java
@@ -0,0 +1,79 @@
+package dev.anilp.ecommerce_backend.entity.phone_number;
+
+import dev.anilp.ecommerce_backend.entity.BaseEntity;
+import dev.anilp.ecommerce_backend.entity.user.User;
+import dev.anilp.ecommerce_backend.validation.EnumValue;
+import dev.anilp.ecommerce_backend.validation.Phone;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "phone_number")
+public class PhoneNumber extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Phone type cannot be null")
+    @EnumValue(enumClass = PhoneType.class, message = "Phone type can be HOME, WORK or MOBILE")
+    @Enumerated(EnumType.STRING)
+    @Column(name = "phone_type", nullable = false, length = 10)
+    private PhoneType phoneType;
+
+    @NotBlank(message = "Phone number cannot be blank.")
+    @Phone
+    @Column(name = "phone_number", nullable = false, unique = true, length = 13)
+    private String phoneNumber;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH}, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, referencedColumnName = "id")
+    private User user;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof PhoneNumber that)) {
+            return false;
+        }
+        return Objects.equals(phoneNumber, that.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(phoneNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "UserPhoneNumber{" +
+                "id=" + id +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", phoneType=" + phoneType +
+                ", userId=" + user.getId() +
+                '}';
+    }
+}
---
+++ src/main/java/dev/anilp/ecommerce_backend/entity/phone_number/PhoneType.java
@@ -0,0 +1,7 @@
+package dev.anilp.ecommerce_backend.entity.phone_number;
+
+public enum PhoneType {
+    HOME,
+    WORK,
+    MOBILE
+}
