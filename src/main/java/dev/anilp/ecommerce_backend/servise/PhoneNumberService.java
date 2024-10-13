16c11c7dedd7b8eed7810651b9a7b06db329201a
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-10-13 21:55:36 +0300

    Add PhoneNumberService to handle phone number operations
    Introduce `PhoneNumberService` with methods to retrieve phone numbers associated with specific users. This service interacts with `PhoneNumberRepository`, `UserService`, and `UserMapper` to provide a streamlined approach to phone number management within the application.

---
+++ src/main/java/dev/anilp/ecommerce_backend/servise/PhoneNumberService.java
@@ -0,0 +1,41 @@
+package dev.anilp.ecommerce_backend.servise;
+
+import dev.anilp.ecommerce_backend.dto.phone_number.PhoneNumberResponseDTO;
+import dev.anilp.ecommerce_backend.entity.phone_number.PhoneNumber;
+import dev.anilp.ecommerce_backend.entity.user.User;
+import dev.anilp.ecommerce_backend.exception.exception_class.ResourceNotFoundException;
+import dev.anilp.ecommerce_backend.mapper.UserMapper;
+import dev.anilp.ecommerce_backend.repository.PhoneNumberRepository;
+import org.springframework.stereotype.Service;
+
+import static dev.anilp.ecommerce_backend.exception.ExceptionConstant.ID;
+import static dev.anilp.ecommerce_backend.exception.ExceptionConstant.PHONE_NUMBER;
+
+@Service
+public class PhoneNumberService {
+    private final PhoneNumberRepository phoneNumberRepository;
+    private final UserService userService;
+    private final UserMapper mapper;
+
+    public PhoneNumberService(PhoneNumberRepository phoneNumberRepository, UserService userService, UserMapper mapper) {
+        this.phoneNumberRepository = phoneNumberRepository;
+        this.userService = userService;
+        this.mapper = mapper;
+    }
+
+    public PhoneNumberResponseDTO getPhoneNumberOfUserById(Long userId, Long phoneNumberId) {
+        User user = userService.findUserById(userId);
+
+        return mapper.phoneNumberToResponse(
+                findUserPhoneNumberById(phoneNumberId, user)
+        );
+    }
+
+    private PhoneNumber findUserPhoneNumberById(Long phoneNumberId, User user) {
+        return user.getPhoneNumbers()
+                .stream()
+                .filter(phoneNumber -> phoneNumber.getId().equals(phoneNumberId))
+                .findFirst()
+                .orElseThrow(() -> new ResourceNotFoundException(PHONE_NUMBER, ID, phoneNumberId.toString()));
+    }
+}
