ba83b971cfa4f008f8bceb8d45d25908eea85a12
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-10-21 13:28:07 +0300

    test(repository): Add PhoneRepositoryTest for phone-related queries
    - Implement tests for existsByPhoneNumber method
    - Add tests for findPhoneByIdAndUserId method
    - Create tests for findPhonesByUserId method
    - Set up test data with users and phones
    - Use H2 database and Flyway for test data management

---
+++ src/test/java/dev/anilp/ecommerce_backend/repository/PhoneRepositoryTest.java
@@ -0,0 +1,143 @@
+package dev.anilp.ecommerce_backend.repository;
+
+import dev.anilp.ecommerce_backend.entity.phone.Phone;
+import dev.anilp.ecommerce_backend.entity.user.User;
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static dev.anilp.ecommerce_backend.entity.phone.PhoneType.MOBILE;
+import static dev.anilp.ecommerce_backend.entity.user.Gender.FEMALE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest(properties = {
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.jpa.properties.jakarta.persistence.validation.mode=none"
+})
+class PhoneRepositoryTest {
+    @Autowired
+    private PhoneRepository phoneRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private Flyway flyway;
+
+    private User userWithPhone;
+    private Phone phone;
+    private User userWithoutPhone;
+
+    @BeforeEach
+    void setUp() {
+        flyway.clean();
+        flyway.migrate();
+        userWithPhone = new User(
+                null,
+                "Test First Name",
+                "Test Last Name",
+                "withphone@test.com",
+                FEMALE,
+                LocalDate.of(1990, 1, 1),
+                new ArrayList<>(), new ArrayList<>()
+        );
+        phone = new Phone(
+                null,
+                MOBILE,
+                "1112223344",
+                null
+        );
+        userWithPhone.addPhone(phone);
+        phoneRepository.save(phone);
+        userWithoutPhone = new User(
+                null,
+                "Test First Name",
+                "Test Last Name",
+                "withoutphone@test.com",
+                FEMALE,
+                LocalDate.of(1990, 1, 1),
+                new ArrayList<>(), new ArrayList<>());
+        userRepository.save(userWithoutPhone);
+    }
+
+    @Nested
+    class ExistsByPhoneNumberTest {
+        @Test
+        void itShouldReturnTrueWhenPhoneNumberExists() {
+            // Given
+            String phoneNumber = "1112223344";
+            // When
+            Boolean result = phoneRepository.existsByPhoneNumber(phoneNumber);
+            // Then
+            assertThat(result).isTrue();
+        }
+
+        @Test
+        void itShouldReturnFalseWhenPhoneNumberNotExists() {
+            // Given
+            String phoneNumber = "1112223345";
+            // When
+            Boolean result = phoneRepository.existsByPhoneNumber(phoneNumber);
+            // Then
+            assertThat(result).isFalse();
+        }
+    }
+
+    @Nested
+    class FindPhoneByIdAndUserIdTest {
+        @Test
+        void itShouldReturnPresentOptionalPhoneWhenPhoneExists() {
+            // Given
+            Long phoneId = phone.getId();
+            Long userId = userWithPhone.getId();
+            // When
+            Optional<Phone> result = phoneRepository.findPhoneByIdAndUserId(phoneId, userId);
+            // Then
+            assertThat(result).isPresent()
+                    .contains(phone);
+        }
+
+        @Test
+        void itShouldReturnEmptyOptionalWhenPhoneNotExists() {
+            // Given
+            Long phoneId = phone.getId();
+            Long userId = userWithoutPhone.getId();
+            // When
+            Optional<Phone> result = phoneRepository.findPhoneByIdAndUserId(phoneId, userId);
+            // Then
+            assertThat(result).isEmpty();
+        }
+    }
+
+    @Nested
+    class FindPhonesByUserIdTest {
+        @Test
+        void itShouldReturnListOfPhonesWhenPhonesExists() {
+            // Given
+            Long userId = userWithPhone.getId();
+            // When
+            List<Phone> result = phoneRepository.findPhonesByUserId(userId);
+            // Then
+            assertThat(result).isNotEmpty()
+                    .containsExactlyElementsOf(userWithPhone.getPhones());
+        }
+
+        @Test
+        void itShouldReturnEmptyListWhenPhonesNotExists() {
+            // Given
+            Long userId = userWithoutPhone.getId();
+            // When
+            List<Phone> result = phoneRepository.findPhonesByUserId(userId);
+            // Then
+            assertThat(result).isEmpty();
+        }
+    }
+}
