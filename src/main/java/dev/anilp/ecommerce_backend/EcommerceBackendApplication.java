e496bf67fe300563b5c514c5f3a8d6768ae2e927
AnilPirincoglu <anl.pirincoglu@gmail.com>
2024-10-23 11:18:55 +0300

    add(config) : new mongo config file added. auditing annotation deleted from main class and add config file.

--- src/main/java/dev/anilp/ecommerce_backend/EcommerceBackendApplication.java
+++ src/main/java/dev/anilp/ecommerce_backend/EcommerceBackendApplication.java
@@ -2,14 +2,11 @@ package dev.anilp.ecommerce_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
-@EnableMongoAuditing
 public class EcommerceBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EcommerceBackendApplication.class, args);
     }
-
 }
---
+++ src/main/java/dev/anilp/ecommerce_backend/config/MongoDbConfig.java
@@ -0,0 +1,9 @@
+package dev.anilp.ecommerce_backend.config;
+
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableMongoAuditing
+public class MongoDbConfig {
+}
